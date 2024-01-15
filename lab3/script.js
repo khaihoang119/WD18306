3de5e580f6e505fcdfd09554f68a3054d1020d86
khaihoang119 <khaihoang119nkh@gmail.com>
2024-01-15 20:48:23 +0700

    lab 3

--- lab3/script.js
+++ lab3/script.js
@@ -18,4 +18,53 @@ let padZero = (num, totalLen) =>{
 }
 console.log(typeof(padZero(2,1)));
 console.log(padZero(10,2));
-                                    
+
+let power = (base, exponent) =>{
+    let result = 1;
+    for(let i  = 0; i < exponent; i++){
+        result *=base;
+    }
+    return result;
+}
+console.log(power(2,10));
+
+let greet = (who) => console.log("Hello " + who);
+console.log(greet("Hoang"));
+
+//Bài 2
+const arr = [1,2,3,4,5,6,7];
+let sum = (array) =>{
+   let sumArray = array.reduce((total, curentValue) => total + curentValue,0 );
+   return sumArray;
+}
+console.log(sum(arr));
+
+//Bài 3
+function Entity(name, delay) {
+    this.name = name;
+    this.delay = delay;
+}
+Entity.prototype.greet = function() {
+    setTimeout(() => {
+        console.log('Xin chào, tên tôi là ', this.name);
+    },this.delay);
+};
+let java = new Entity('Java');
+let cpp = new Entity ('C++');
+java.greet();
+cpp.greet();
+
+//Bài 4
+let convertTemperature = (temperature, unit) =>{
+    if(unit === "C"){
+        return (temperature *9/5)+32 +" C";
+    }else if(unit === "F"){
+        return (temperature - 32) *5/9 +" F";
+    }else{
+        console.log("Đơn vị nhiệt độ không hợp lệ. Chỉ nhập 'C' hoặc 'F'.");
+        return null;
+    }
+}
+console.log(convertTemperature(30, "C")); 
+console.log(convertTemperature(100, "F")); 
+console.log(convertTemperature(25, "K")); 
