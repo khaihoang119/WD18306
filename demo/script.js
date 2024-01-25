ae63752cc8f03d1c876d309b18016583b39bdc88
Johhny <khaihoang119nkh@gmail.com>
2024-01-25 15:47:10 +0700

    lab5

--- demo/script.js
+++ demo/script.js
@@ -1,5 +1,14 @@
-const p = document.querySelector('.p');
-setTimeout(function () {
-    p.textContent = 'My name is Jonas!';
-}, 5000);
-p.style.color = 'red';
+// const p = document.querySelector('.p');
+// setTimeout(function () {
+//     p.textContent = 'My name is Jonas!';
+// }, 5000);
+// p.style.color = 'red';
+
+class Person {
+    constructor(name) {
+    this.name = name;
+    }
+    }
+    
+    const member = new Person('John');
+    console.log(typeof member);
