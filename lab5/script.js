1762715ef6284543afac07605d19be79cd8d3220
khaihoang119 <khaihoang119nkh@gmail.com>
2024-01-29 22:53:25 +0700

    lab 5 và asm

--- lab5/script.js
+++ lab5/script.js
@@ -1,71 +1,168 @@
-const { default: axios } = require("axios");
-const API_URL = 'http://localhost:3000/';
-axios.get(API_URL + "comments").then((data) => console.log(data.data));
-axios.get(API_URL + "comments").then(({ data }) => console.log(data));
+//Bài 1
+//this Trong JavaScript, "this" thường được sử dụng để tham chiếu đến đối tượng hiện tại trong ngữ cảnh thực thi.
+let student = {
+    name: "Khải Hoàng",
+    sayHello: function () {
+        console.log("Hello, " + this.name);
+    }
+};
 
-// let comment = {
-//     "content": "Nice!!!",
-//     "postId": 1
-// }
+student.sayHello();
 
+//Bài 2
+//Chuyển đổi
+// Shape.prototype.move = function(x,y){
+//     this.x = x;
+//     this.y = y;
+// };
+
+class Shape {
+    move(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+const myShape = new Shape();
+myShape.move(5, 10);
+console.log(myShape.x, myShape.y);
+
+//Bài 3
+class Clock {
+    constructor({ template }) {
+        this.template = template;
+        this.timer;
+    }
+    render() {
+        let date = new Date();
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
+    }
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
+
+    }
+}
+let clock = new Clock({ template: 'h:m:s' });
+clock.start();
+setTimeout(() => {
+    clock.stop(); // Dừng đồng hồ sau một khoảng thời gian
+}, 5000); // Dừng sau 5 giây
+
+//Bài 4
+var person = {
+    firstname: "Nguyễn",
+    lastname: "Khải Hoàng",
+    set lastname(value) {
+        this._lastname = value;
+    },
+    get lastname() {
+        return this._lastname;
+    },
+    set firstname(value) {
+        this._firstname = value;
+    },
+    get firstname() {
+        return this._firstname;
+    },
+    getFullName: function () {
+        return `${this._firstname} ${this._lastname}`;
+    }
+};
+person.lastname ="Hoàng";
+person.firstname = "Khải";
+console.log(person.getFullName());
+
+//Bài 5
 class APICaller {
     constructor(baseUrl) {
-        this.baseUrl = baseUrl;
+      this.baseUrl = baseUrl;
     }
+  
     async get(endpoint) {
-        try {
-            return axios.get(`${this.baseUrl}${endpoint}`);
-        } catch (error) {
-            console.log('Error data', error);
-        }
+      try {
+        const response = await fetch(`${this.baseUrl}/${endpoint}`);
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+      }
     }
-}
-
-class Comment extends APICaller {
-    constructor(baseUrl, commentId) {
-        super(baseUrl);
-        // this.comment = comment;
-        this.commentId = commentId;
+  }
+  
+  class Comment {
+    constructor(apiCaller) {
+      this.apiCaller = apiCaller;
     }
+  
     async getAll() {
-        try {
-            return  this.API_URL.get('comments')
-        } catch (error) {
-            console.log('Error data', error);
-        }
+      try {
+        const comments = await this.apiCaller.get('comments');
+        return comments;
+      } catch (error) {
+        throw error;
+      }
     }
+  
     async getOne(commentId) {
-        return await this.API_URL.get(`comments/${commentId}`);
-    } catch(error) {
-        console.log('Error data', error);
+      try {
+        const comment = await this.apiCaller.get(`comments/${commentId}`);
+        return comment;
+      } catch (error) {
+        throw error;
+      }
     }
-}
-class Post extends APICaller {
-    constructor(baseUrl, postId) {
-        super(baseUrl);
-        // this.posts = posts;
-        this.postId = postId;
-        this.endpoint = 'post';
+  }
+  
+  class Post {
+    constructor(apiCaller) {
+      this.apiCaller = apiCaller;
     }
+  
     async getAll() {
-        try {
-            return this.API_URL.get('post')
-        } catch (error) {
-            console.log("Error data", error);
-        }
+      try {
+        const posts = await this.apiCaller.get('posts');
+        return posts;
+      } catch (error) {
+        throw error;
+      }
     }
+  
     async getOne(postId) {
-        try {
-            return await this.API_URL.get(`posts/${postId}`);
-        }catch (error) {
-            console.log("Error data", error);
-        }
+      try {
+        const post = await this.apiCaller.get(`posts/${postId}`);
+        return post;
+      } catch (error) {
+        throw error;
+      }
     }
-}
-
-// comment = new Comment("comments","nice",1);
-// console.log(comment)
-const commentService = new Comment(API_URL);
-// const postService = new Post(API_URL);
-
-commentService.getAll().then(comments =>console.log('Comments',comments));
+  }
+  
+// Kiểm tra in ra console log
+  
+  const apiCaller = new APICaller('http://localhost:3000');
+  
+  const commentAPI = new Comment(apiCaller);
+  commentAPI.getAll().then((comments) => console.log('All comments:', comments));
+  
+  const postAPI = new Post(apiCaller);
+  postAPI.getOne(1).then((post) => console.log('Post with ID 1:', post));
+  
---
+++ lab5_demo/script.js
@@ -0,0 +1,70 @@
+const { default: axios } = require("axios");
+const API_URL = 'http://localhost:3000/';
+axios.get(API_URL + "comments").then((data) => console.log(data.data));
+axios.get(API_URL + "comments").then(({ data }) => console.log(data));
+
+// let comment = {
+//     "content": "Nice!!!",
+//     "postId": 1
+// }
+
+class APICaller {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+    async get(endpoint) {
+        try {
+            return axios.get(`${this.baseUrl}${endpoint}`);
+        } catch (error) {
+            console.log('Error data', error);
+        }
+    }
+}
+
+class Comment extends APICaller {
+    constructor(baseUrl, commentId) {
+        super(baseUrl);
+        // this.comment = comment;
+        this.commentId = commentId;
+    }
+    async getAll() {
+        try {
+            return  this.API_URL.get('comments')
+        } catch (error) {
+            console.log('Error data', error);
+        }
+    }
+    async getOne(commentId) {
+        return await this.API_URL.get(`comments/${commentId}`);
+    } catch(error) {
+        console.log('Error data', error);
+    }
+}
+class Post extends APICaller {
+    constructor(baseUrl, postId) {
+        super(baseUrl);
+        // this.posts = posts;
+        this.postId = postId;
+    }
+    async getAll() {
+        try {
+            return this.API_URL.get('post')
+        } catch (error) {
+            console.log("Error data", error);
+        }
+    }
+    async getOne(postId) {
+        try {
+            return await this.API_URL.get(`posts/${postId}`);
+        }catch (error) {
+            console.log("Error data", error);
+        }
+    }
+}
+
+// comment = new Comment("comments","nice",1);
+// console.log(comment)
+const commentService = new Comment(API_URL);
+// const postService = new Post(API_URL);
+
+commentService.getAll().then(comments =>console.log('Comments',comments));
