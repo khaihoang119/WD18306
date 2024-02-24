062659c9c135ea61526fe47ddeb91f34f2308e85
khaihoang110 <khaihoang119nkh@gmail.com>
2024-02-24 14:46:22 +0700

    asm

--- ASM/modules/products/all_products.js
+++ ASM/modules/products/all_products.js
@@ -8,13 +8,13 @@ export function fetchData() {
 	return new Promise((resolve, reject) => {
 		axios.get(`${API_URL}products?cate_id=`)
 			.then(products => {
-       
+
 				for (let i = 0; i < products.length; i++) {
 					const product = products[i];
 					console.log(product.name);
 					console.log(product.price);
 					// ...
-				} 
+				}
 				// Kiểm tra nếu response không thành công (status code không phải 200)
 				if (products.status !== 200) {
 					throw new Error(`Failed to fetch data. Status: ${products.status}`);
